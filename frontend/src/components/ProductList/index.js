ac16a8541bf4268fd38b3b539ad2fa7975250afa
bobruk76 <vladimir.m.polyakov@gmail.com>
2021-09-07 16:44:49 +0300

    new day

---
+++ frontend/src/components/ProductList/index.js
@@ -0,0 +1,22 @@
+export default {
+  props: {
+
+  },
+
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+
+    },
+
+  methods: {
+
+  },
+
+  created() {
+
+  },
+};
---
+++ frontend/src/components/ProductList/template.vue
@@ -0,0 +1,9 @@
+<template>
+
+</template>
+
+<style lang="stylus" scoped>
+  @import "./styles.styl";
+</style>
+
+<script src="./index.js"></script>
