478eaf66967755b650b6825846b1a07c0721595c
bobruk76 <vladimir.m.polyakov@gmail.com>
2021-09-08 16:07:51 +0300

    add products

--- frontend/src/data/products.js
+++ frontend/src/data/products.js
@@ -14,4 +14,39 @@ export default [
     price: 21790,
     img: 'img/phone.jpg',
   },
+  {
+    title: 'Электроскейт Razor Cruiser',
+    price: 24690,
+    img: 'img/board.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi 6/128GB',
+    price: 8960,
+    img: 'img/phone-3.jpg',
+  },
+  {
+    title: 'Электрический дрифт-карт Razor Crazy Cart',
+    price: 39900,
+    img: 'img/bicycle.jpg',
+  },
+  {
+    title: 'Гироскутер Razor Hovertrax 2.0',
+    price: 34900,
+    img: 'img/wheels.jpg',
+  },
+  {
+    title: 'Детский трюковой самокат Razor Grom',
+    price: 4990,
+    img: 'img/scooter.jpg',
+  },
+  {
+    title: 'Роллерсёрф Razor RipStik Air Pro',
+    price: 6690,
+    img: 'img/ripstik.jpg',
+  },
+  {
+    title: 'Наушники AirPods с беспроводным зарядным футляром',
+    price: 16560,
+    img: 'img/airpods.jpg',
+  },
 ];
