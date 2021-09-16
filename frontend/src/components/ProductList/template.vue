e91d5592dc077cc6151609f4c267d85a02d37599
bobruk76 <vladimir.m.polyakov@gmail.com>
2021-09-16 11:47:50 +0300

    start make paginate

--- frontend/src/components/ProductList/template.vue
+++ frontend/src/components/ProductList/template.vue
@@ -1,6 +1,6 @@
 <template>
     <ul class="catalog__list">
-      <li class="catalog__item" v-for="(product, index) in products" :key="index">
+      <li class="catalog__item" v-for="product in products" :key="product.id">
         <a class="catalog__pic" href="#">
           <img :src="product.img" alt="Название товара">
         </a>
